2f5b5e809cd9481a90ad93ef493d4aa5e1e13b5f
Alec Campos Aoki <alec.camposaoki@gmail.com>
2026-04-10 13:46:44 -0300

    Add volunteer forms to Dashboard

---
+++ src/pages/Dashboard/ManageVolunteers.tsx
@@ -0,0 +1,18 @@
+function ManageVolunteers() {
+  return (
+    <div className="w-full border-[3px] border-black shadow-[8px_8px_0px_0px_rgba(0,0,0,1)]" style={{ height: "80vh" }}>
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSdCR9_blnxsvgQbfLj6198etfzDXkNca6T2wx46eQ9F0HwN_Q/viewform?embedded=true"
+        width="100%"
+        height="100%"
+        frameBorder={0}
+        marginHeight={0}
+        marginWidth={0}
+      >
+        Carregando…
+      </iframe>
+    </div>
+  );
+}
+
+export default ManageVolunteers;
