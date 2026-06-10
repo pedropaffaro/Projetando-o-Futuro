07b88f028c5231ef863bdfd489b4a6754eb8884a
Alec Campos Aoki <alec.camposaoki@gmail.com>
2026-06-10 10:45:55 -0300

    uhhh

--- backend/handlers/auth.go
+++ backend/handlers/auth.go
@@ -15,30 +15,30 @@ func Login(c *gin.Context) {
 	token, _ := providers.GenerateToken(user.Username)
 	c.JSON(http.StatusOK, gin.H{"token": token})
 
-	// var req models.LoginRequest
-	// passwordProvider := providers.NewBcryptProvider()
+	var req models.LoginRequest
+	passwordProvider := providers.NewBcryptProvider()
 
-	// if err := c.ShouldBindJSON(&req); err != nil {
-	// 	c.JSON(http.StatusBadRequest, gin.H{"error": "Dados inválidos"})
-	// 	return
-	// }
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Dados inválidos"})
+		return
+	}
 
-	// var user models.User
-	// if err := database.DB.Where("username = ?", req.Username).First(&user).Error; err != nil {
-	// 	c.JSON(http.StatusUnauthorized, gin.H{"error": "Credenciais inválidas"})
-	// 	return
-	// }
+	var user models.User
+	if err := database.DB.Where("username = ?", req.Username).First(&user).Error; err != nil {
+	 	c.JSON(http.StatusUnauthorized, gin.H{"error": "Credenciais inválidas"})
+	 	return
+	}
 
-	// if err := passwordProvider.Compare(user.Password, req.Password); err != nil {
-	// 	c.JSON(http.StatusUnauthorized, gin.H{"error": "Credenciais inválidas"})
-	// 	return
-	// }
+	if err := passwordProvider.Compare(user.Password, req.Password); err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Credenciais inválidas"})
+	 	return
+	}
 
-	// token, err := providers.GenerateToken(user.Username)
-	// if err != nil {
-	// 	c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao gerar token"})
-	// 	return
-	// }
+	token, err := providers.GenerateToken(user.Username)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao gerar token"})
+	 	return
+	}
 
-	// c.JSON(http.StatusOK, gin.H{"token": token})
+	c.JSON(http.StatusOK, gin.H{"token": token})
 }
